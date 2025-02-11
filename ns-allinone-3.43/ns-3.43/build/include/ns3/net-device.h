402c368ec2eb2718f71eb999ffd96ee416e6a922
yusrA312 <138700502+yusrA312@users.noreply.github.com>
2025-02-11 19:14:14 +0300

    Delete ns-allinone-3.43 directory

--- ns-allinone-3.43/ns-3.43/build/include/ns3/net-device.h
+++
@@ -1,1 +0,0 @@
-#include "/home/asiaeasa/RM_MEC_and_5G_FYP2025/ns-allinone-3.43/ns-3.43/src/network/model/net-device.h"
--- ns-allinone-3.43/ns-3.43/build/include/ns3/probabilistic-v2v-channel-condition-model.h
+++
@@ -1,1 +0,0 @@
-#include "/home/asiaeasa/RM_MEC_and_5G_FYP2025/ns-allinone-3.43/ns-3.43/src/propagation/model/probabilistic-v2v-channel-condition-model.h"
